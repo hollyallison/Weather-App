8cccd4998ba3c7371a91b1276a42f9b59ecba323
hollyallison <Holly__Allison@hotmail.co.uk>
2023-10-22 18:55:07 +0100

    week 7 changes

--- script.js
+++ script.js
@@ -21,27 +21,58 @@ function updateDayTime() {
 updateDayTime();
 setInterval(updateDayTime, 60000);
 
+function search(city) {
+  let apiKey = "2c40a31a9bebb30oc02aftf7a42a8b2e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
 function displayWeatherCondition(response) {
   document.querySelector("#city-name").innerHTML = response.data.city;
   document.querySelector("#temperature").innerHTML = `${Math.round(
     response.data.temperature.current
-  )}°C`;
+  )}`;
+  celsiusTemperature = response.data.temperature.current;
+  fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   document.querySelector(
-    ".weather-info #Precipitation"
-  ).innerHTML = `Precipitation: ${response.data.condition.description}`;
+    "#Condition"
+  ).innerHTML = ` ${response.data.condition.description}`;
   document.querySelector(
     ".weather-info #Humidity"
   ).innerHTML = `Humidity: ${response.data.temperature.humidity}%`;
   document.querySelector(".weather-info #Wind").innerHTML = `Wind: ${Math.round(
     response.data.wind.speed
   )}m/s`;
+let iconElement = document.querySelector("#weather-icon");
+iconElement.setAttribute(
+  "src",
+  `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+); 
+  }
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-function search(city) {
-  let apiKey = "2c40a31a9bebb30oc02aftf7a42a8b2e";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  fahrenheitLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
+let celsiusTemperature = null;
+let fahrenheitTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 
 function response(event) {
   event.preventDefault();
